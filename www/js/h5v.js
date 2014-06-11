75b151307949ac58139897c5250267b3840fe5c3
snow6oy <snow6oy@fnarg.net>
2014-06-11 12:25:40 +0100

    removed jquery and integrated with uber.js

--- www/js/h5v.js
+++ www/js/h5v.js
@@ -1,99 +1,37 @@
 /* vids=[{},{},{}] id=0..2 */
 var vids, id;
+var u=new uber('/playlists/');
 /* TODO refactor the API so the client can use "action":"replace" to create H5VTags
  * source, title and caption are all "action":"read"
  * OR just this array from the form.elements[]
  **/
 var h5vTags=["source","title","artist","album","rating","trackNumber","producer","genre"];
 
-$(document).ready(
-  /* after search button is clicked fetch the playlist */
-  $('.button').click(function(){
-    var url='/playlists/';
-    if($('.query').val()){
-      url+='search?term='+ $('.query').val();
-    }
-    // console.log("url "+ url);    
-    $.ajax({
-      url:url,
-      dataType:"text",
-      success:function(data){
-        var json=$.parseJSON(data);
-        var sources=captions=items='';
-        /* uber structures are a bit obtuse: transformation is so i can think of a posterLink as 
-           json.uber.playlist[i].url  NOT json.uber.data[0].data[0].url; */
-        var links=flattenLinks(json.uber.data[0]);
-	      window.vids=flattenItems(json.uber.data[1]);
-	      window.vids.forEach(function(i){
-          sources+='<source src="'+ i.source+ '" type="'+ i.type+ '">';
-          captions+='<a href="'+ i.source+ '"><img src="'+ i.caption+ '" alt="'+ i.title+ '"></a>';
-  	    });
-        if(window.vids.length>0){
-          $('#video_player').html('<video controls poster='+ links.poster.url+ '>'+ sources+ '</video>'+ '<figcaption>'+ captions+ '</figcaption>');
-          $("#h5v").attr("action", links.add.url);
-          handleLinks();
-        }else{ // nuke anything displayed from previous search
-          $('#video_player').html('');
-          updateForm();
-          /* var pluralised=(window.vids.length==1) ? 'video' : 'videos';
-          $('#search_results').html('Found '+ window.vids.length+ ' '+ pluralised); */
-          $('#search_results').html('No videos found');
-        }
-      }
-    });
-  }),
-  /* after update button then submit the form */
-  $("#h5v").submit(function(e){
-    var json={};
-    /* copy the user-entered form data */ 
-    for(var i=0;i<h5vTags.length;i++){
-      json[h5vTags[i]]=document.getElementById(h5vTags[i]).value;
-    }
-    /* add the static file reference */
-    json.source=$('#source').html();
-    //alert("Handler called."+ json.source);
-    // remember for after form submitted
-    window.vids[window.id]=json;
-/* TODO it like this var json=vids[id]; 
-   validate source==defined and that rating/trackNumber are integers
-    json.title=document.getElementById('title').value;
-*/
-    e.preventDefault(); // dont send the form via cgi
-    $.ajax({
-      url:'/playlists/',
-      type:'put',
-      dataType:'json',
-      data:JSON.stringify(json),
-      success:function(response){
-        console.log('it worked '+ response.body); 
-      },
-      error:function(thing,desc){
-        console.log('not good '+ desc);
-      }
-    });
-  })
-);
-
+function show(elementID){(
+  function(style){style.display='block';})(document.getElementById(elementID).style);
+}
+function hide(elementID){(
+  function(style){style.display='none';})(document.getElementById(elementID).style);
+}
 function handleLinks(){
-  var video_player=document.getElementById("video_player");
+  var video_player=document.getElementById('video_player');
   var links=video_player.getElementsByTagName('a');
   for(var i=0;i<links.length;i++){
     links[i].onclick=handler;
   }
 }
-
 function handler(e){
   e.preventDefault(); // don't follow the a tag
-  videotarget=$(this).attr("href");
+  videotarget=this.getAttribute("href");
   filename=videotarget.substr(0,videotarget.lastIndexOf('.'))||videotarget;
   video=document.querySelector("#video_player video");
-  //video=$("#video_player video").eq(0);
-  // console.log("fn "+ filename+ " video "+ video);  
+  /*
+  video.removeAttribute("controls");
+  video.setAttribute("poster","file:///C:/Users/gavinj/github/vplayer/www/images/video-placeholder.jpg");
+  */
   source=document.querySelectorAll("#video_player video source");
-  /* for this to work every video must have support for each format */
   source[0].src=filename+".mp4";
   source[1].src=filename+".webm";  // Exiftool can't update webm
-  source[2].src=filename+".3gp";   // wrong mimetype
   video.load();
   video.play();
   /* lookup the selected vid using the href as a key */
@@ -103,20 +41,8 @@ function handler(e){
     }
   }
   updateForm();
-  $("#h5v").show(); // display the updated form
+  show('update_metadata');
 }
-
-/* TODO rewrite this the jQuery way $.(#h5v).html(htmlString);
-function updateText(link) {
-  var x; 
-  for (var i=0;i<vids.length;i++) {
-    if (vids[i].source==link) {
-      x=i; // index to lookup vid attributes
-      //console.log(vids[i].source+ ' <> '+ link);
-    }
-  }
-*/
-
 function updateForm() {
   var x=window.id; // convenience
   // but what about the placholders?
@@ -131,69 +57,63 @@ function updateForm() {
   document.getElementById('caption').innerHTML=window.vids[x].caption;  
 }
 
-
-function XXXupdateForm() {
-  var x=window.id; // convenience
-  document.getElementById('source').innerHTML=window.vids[x].source;
-  document.getElementById('type').innerHTML=window.vids[x].type;
-  document.getElementById('caption').innerHTML=window.vids[x].caption;
-  // only override the placeholders if there is content
-  if (window.vids[x].title && window.vids[x].title.length) {
-    document.getElementById('title').value=window.vids[x].title;
-  } 
-  if (window.vids[x].artist && window.vids[x].artist.length) {
-    document.getElementById('artist').value=window.vids[x].artist;
+window.addEventListener("load", function(){
+  // allowed values: [-1..5]    well ok -5 should not be allowed but it is *shrug*
+  function ratingIsNumber(rating){
+    //var reg=new RegExp("^[-]?[0-9]+[\.]?[0-9]+$");
+    var reg=new RegExp("^[-]?[0-5]$");    
+    return reg.test(rating);
   }
-  if (window.vids[x].album && window.vids[x].album.length) {
-    document.getElementById('album').value=window.vids[x].album;
+  function trackIsNumber(trackNumber){
+    var reg=new RegExp("^[0-9]+$");  // any positive integer    
+    return reg.test(trackNumber);
   }
-  if (window.vids[x].rating && window.vids[x].rating.length) {
-    document.getElementById('rating').value=window.vids[x].rating;
-  }
-  if (window.vids[x].trackNumber && window.vids[x].trackNumber.length) {
-    document.getElementById('trackNumber').value=window.vids[x].trackNumber;
-  }
-  if (window.vids[x].producer && window.vids[x].producer.length) {
-    document.getElementById('producer').value=window.vids[x].producer;
-  }
-  if (window.vids[x].genre && window.vids[x].genre.length) {
-    document.getElementById('genre').value=window.vids[x].genre;
-  }
-}
-
-function flattenItems(uber) {
-  var label;
-  var items=[];
-  uber.data.forEach(function(uber) {
-     if (uber["id"]) {
-        // come back later please
-     } else {
-        uber.forEach(function(uber) {
-           if (uber["data"]) {
-              var x={};            
-              uber.data.forEach(function(item) {
-                if (undefined==item) {
-                  x[label]=null;
-                } else if (item.name) {
-                  label=item.name;
-                } else {
-                  x[label]=item;
-                }
-              })
-              items.push(x);
-           }
-        });
-     }
+  /*
+   * READ
+   */
+  var button=document.getElementById('search');
+  button.addEventListener("click", function (event){
+    var filter=document.getElementById('filter').value;
+    u.read(filter, function(){
+      window.vids=this.items; // make global for later
+      var sources=captions='';
+      this.items.forEach(function (i){
+        sources+='<source src="'+ i.source+ '" type="'+ i.type+ '">';
+        captions+='<a href="'+ i.source+ '"><img src="'+ i.caption+ '" alt="'+ i.title+ '"></a>';
+      });
+      if(this.items.length>0){
+        document.getElementById('video_player').innerHTML='<video controls poster='+ this.links.poster.url+ '>'+ sources+ '</video>'+ '<figcaption>'+ captions+ '</figcaption>';
+        /* TODO is it null because its hidden?
+                console.log("action link is "+ this.links.add.url);
+        document.querySelector('update_metadata').setAttribute("action", this.links.add.url); */
+        handleLinks();
+      }else{ // nuke anything displayed from previous search
+        document.getElementById('video_player').innerHTML='';
+        updateForm();
+        /* var pluralised=(window.vids.length==1) ? 'video' : 'videos';
+        $('#search_results').html('Found '+ window.vids.length+ ' '+ pluralised); */
+        document.getElementById('video_player').innerHTML='No videos found';
+      }
+    });
   });
-  return items;
-}
-
-function flattenLinks(uber){
-  var links={};
-  uber.data.forEach(function(link) {
-    var key=link.id;
-    delete link["id"];
-    links[key]=link;
+  /*
+   * UPDATE
+   */
+  var form=document.getElementById("update_metadata");
+  form.addEventListener("submit", function (event){
+    event.preventDefault();
+    if(! ratingIsNumber(document.getElementById('rating'))){
+      document.getElementById('rating').value=-1; // revert to unrated
+    }
+    if(! trackIsNumber(document.getElementById("trackNumber"))){
+      document.getElementById('trackNumber').value=0; // trackNumber='thirteen' will raise a server error
+    }
+    u.update(form, function(){
+      if(this.status==200){  // TODO use a switch 200 400 500 etc.
+        console.log("response "+ this.response.body);
+      }else{
+        console.log("response '"+ this.status+ "' fnarg!");
+      }
+    });
   });
-  return links;
-}
+});
