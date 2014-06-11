6b27d11379dd0e78e8343217d1e5ac4adfb48ab4
snow6oy <snow6oy@fnarg.net>
2014-06-11 12:24:07 +0100

    Separated diplay and connector logic

--- www/js/uber.js
+++ www/js/uber.js
@@ -1,21 +1,39 @@
 // CRUD wrapper for uber hypermedia
 function uber(url){
   this.homeUrl=url;
+//  this.links;
+//  this.items=["one","two"];
+//  var items;
   //this.tags=["title","artist","album","rating","trackNumber","producer","genre"];
   // search with a filter or leave empty for random
-  this.read=function(filter){
-    if(arguments.length){
-      console.log('Searching for '+ filter+ ' with this '+ this.homeUrl);
-    }else{
-      console.log('Being random with this '+ this.homeUrl);
+  this.read=function(filter, responseHandler){
+    var xhr=new XMLHttpRequest();
+    var data={};
+    (filter) ?
+      xhr.open('GET', this.homeUrl+ 'search?term='+ filter) :
+      xhr.open('GET', this.homeUrl); // returns a random selection
+    xhr.onreadystatechange=function(){
+      if(this.readyState==4){  // readyState 4 means "complete"
+        if(this.status==200){  // TODO use a switch 200 400 500 etc.
+          response=JSON.parse(xhr.responseText);
+          // add version too ..
+          data.links=flattenLinks(response.uber.data[0]);
+          data.items=flattenItems(response.uber.data[1]);
+          responseHandler.call(data);
+        }else{
+          console.log("response '"+ this.status+ "' fnarg!");
+          return false;
+        }
+      }
     }
+    xhr.send(null); // no payload
   };
   this.create=function(){ // either upload a new asset OR promote an existing asset
   };
-  this.update=function(mdat){ // replace metadata
+  this.update=function(form, responseHandler){ // replace metadata
     var payload={};
-    for(var i=0;i<mdat.length;i++){
-      elem=mdat.elements[i];
+    for(var i=0;i<form.length;i++){
+      elem=form.elements[i];
       if(elem.id){ // skip elements without ids, e.g. the submit button
         payload[elem.id]=elem.value;
       }
@@ -24,16 +42,47 @@ function uber(url){
     xhr.open('PUT', this.homeUrl);
     xhr.onreadystatechange=function(){
       if(this.readyState==4){
-        if(this.status==200){  // TODO use a switch 200 400 500 etc.
-          response=JSON.parse(xhr.responseText);
-          console.log("response "+ response.body);
-        }else{
-          console.log("response '"+ this.status+ "' fnarg!");
-        }
+        this.response=JSON.parse(xhr.responseText);
+        responseHandler.call(this);
       }
     }
     xhr.send(JSON.stringify(payload));
   };
   // delete means move item to archived folder
   this.delete=function(){};
+  function flattenItems(uber) {
+    var label;
+    var items=[];
+    uber.data.forEach(function (uber) {
+       if (uber["id"]) {
+          // come back later please
+       } else {
+          uber.forEach(function (uber) {
+             if (uber["data"]) {
+                var x={};
+                uber.data.forEach(function (item) {
+                  if (undefined==item) {
+                    x[label]=null;
+                  } else if (item.name) {
+                    label=item.name;
+                  } else {
+                    x[label]=item;
+                  }
+                })
+                items.push(x);
+             }
+          });
+       }
+    });
+    return items;
+  }
+  function flattenLinks(uber){
+    var links={};
+    uber.data.forEach(function (link) {
+      var key=link.id;
+      delete link["id"];
+      links[key]=link;
+    });
+    return links;
+  }
 };
